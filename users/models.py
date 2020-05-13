4a0af95036b531d8b7fc5172cb6cbe4188ea3272
Alex <aixongaming@gmail.com>
2020-05-13 09:26:28 +0200

    make it better

--- users/models.py
+++ users/models.py
@@ -4,8 +4,11 @@ from PIL import Image
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
-	#address = models.CharField(max_length = 100, default = 'adresse')
+	address = models.CharField(max_length = 255, verbose_name = _("Address"))
+	city = models.CharField(max_length = 255, verbose_name = _("City"))
+	postal = models.CharField(max_length = 255, verbose_name = _("Postal code"))
 	image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
 	
 	def __str__(self):
 		return '{self.user.username} Profile'
+
